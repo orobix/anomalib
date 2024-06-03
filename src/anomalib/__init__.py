b3fbbcfb8a226f7b72659787e024a30873ddab9a
Lorenzo Mammana <lorenzo.mammana@orobix.com>
2024-06-03 15:33:32 +0200

    build: Update version, update changelog

--- src/anomalib/__init__.py
+++ src/anomalib/__init__.py
@@ -4,6 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 anomalib_version = "0.7.0"
-custom_orobix_version = "1.3.2"
+custom_orobix_version = "1.3.3"
 
 __version__ = f"{anomalib_version}+obx.{custom_orobix_version}"
